6c5bab6cc71cc05a27d42235d58351317456da2d
sokrasins <sokrasins@gmail.com>
2017-01-02 00:14:40 -0500

    Added basic volume animation

--- src/app/components/AudioPlayer.jsx
+++ src/app/components/AudioPlayer.jsx
@@ -1,17 +1,36 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
 class AudioPlayer extends React.Component {
   constructor(props) {
     super(props)
     this.arcadeSound = require('file!./../audio/arcade92.mp3');
+    this.changeVolume = this.changeVolume.bind(this)
+  }
+
+  changeVolume(audioNode, targetVol) {
+    let vol = audioNode.volume 
+    // TODO: Change growth from linear to exponential
+    vol = (targetVol>vol ? Math.min(vol+0.005, targetVol) : Math.max(vol-0.005, targetVol))
+
+    audioNode.volume = vol
+    const stillGoing = (targetVol>vol ? vol<targetVol : vol>targetVol)
+    if (stillGoing) {
+      setTimeout(() => { this.changeVolume(audioNode, targetVol) }, 100)
+    }
+  }
+
+  componentDidMount() {
+    let audioNode = ReactDOM.findDOMNode(this.refs.soundfile)
+    audioNode.volume = 0
+    setTimeout(() => { this.changeVolume(audioNode, 1.0) }, 100)
   }
 
   render () {
-    console.log('props: ', this.props)
     return (
-      <audio ref='audio' muted={!this.props.audio} autoPlay loop> 
+      <audio ref='audio' ref="soundfile" muted={!this.props.audio} autoPlay loop> 
         <source src={this.arcadeSound} type="audio/mpeg" />
-        Your browser does not support the audio content
+          Your browser does not support the audio content
       </audio>
     )
   }
@@ -19,4 +38,3 @@ class AudioPlayer extends React.Component {
 
 export default AudioPlayer
 
-// TODO: Add fading
