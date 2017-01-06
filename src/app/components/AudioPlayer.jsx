100fcc8bcbeaa990e1e422f264492ba31fd3bfdc
sokrasins <sokrasins@gmail.com>
2017-01-05 22:44:10 -0500

    played with audio fading timing

--- src/app/components/AudioPlayer.jsx
+++ src/app/components/AudioPlayer.jsx
@@ -11,19 +11,20 @@ class AudioPlayer extends React.Component {
   changeVolume(audioNode, targetVol) {
     let vol = audioNode.volume 
     // TODO: Change growth from linear to exponential
-    vol = (targetVol>vol ? Math.min(vol+0.005, targetVol) : Math.max(vol-0.005, targetVol))
+    vol = (targetVol>vol ? Math.min(vol+0.002, targetVol) : Math.max(vol-0.002, targetVol))
 
     audioNode.volume = vol
+    console.log(vol)
     const stillGoing = (targetVol>vol ? vol<targetVol : vol>targetVol)
     if (stillGoing) {
-      setTimeout(() => { this.changeVolume(audioNode, targetVol) }, 100)
+      setTimeout(() => { this.changeVolume(audioNode, targetVol) }, 10)
     }
   }
 
   componentDidMount() {
     let audioNode = ReactDOM.findDOMNode(this.refs.soundfile)
     audioNode.volume = 0
-    setTimeout(() => { this.changeVolume(audioNode, 1.0) }, 100)
+    setTimeout(() => { this.changeVolume(audioNode, 1.0) }, 10)
   }
 
   render () {
