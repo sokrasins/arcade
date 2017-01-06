d89fdf5f4cf91d39ca127b617cb056948ef95c94
sokrasins <sokrasins@gmail.com>
2017-01-05 23:08:08 -0500

    implemented exponential volume fading

--- src/app/components/AudioPlayer.jsx
+++ src/app/components/AudioPlayer.jsx
@@ -1,38 +1,53 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// TODO: Drive all audio volume levels with redux state
 class AudioPlayer extends React.Component {
   constructor(props) {
     super(props)
-    this.arcadeSound = require('file!./../audio/background.mp3');
+    this.audio = {
+      'background': require('file!./../audio/background.mp3'),
+      'machine': require('file!./../audio/machines.mp3')
+    }
     this.changeVolume = this.changeVolume.bind(this)
   }
 
-  changeVolume(audioNode, targetVol) {
+  // TODO: Change input params to: ([startVol], endVol, period)
+  changeVolume(audioNode, curVol, targetVol) {
     let vol = audioNode.volume 
-    // TODO: Change growth from linear to exponential
-    vol = (targetVol>vol ? Math.min(vol+0.002, targetVol) : Math.max(vol-0.002, targetVol))
+    curVol = (targetVol>curVol ? Math.min(curVol+0.002, targetVol) : Math.max(curVol-0.002, targetVol))
 
-    audioNode.volume = vol
-    console.log(vol)
-    const stillGoing = (targetVol>vol ? vol<targetVol : vol>targetVol)
+    audioNode.volume = (Math.exp(curVol)-1)/(Math.E-1)
+    console.log(audioNode.volume)
+    const stillGoing = (targetVol>curVol ? curVol<targetVol : curVol>targetVol)
     if (stillGoing) {
-      setTimeout(() => { this.changeVolume(audioNode, targetVol) }, 10)
+      setTimeout(() => { this.changeVolume(audioNode, curVol, targetVol) }, 20)
     }
   }
 
   componentDidMount() {
-    let audioNode = ReactDOM.findDOMNode(this.refs.soundfile)
-    audioNode.volume = 0
-    setTimeout(() => { this.changeVolume(audioNode, 1.0) }, 10)
+    let machineNode = ReactDOM.findDOMNode(this.refs.machine)
+    machineNode.volume = 0
+    setTimeout(() => { this.changeVolume(machineNode, 0.0, 0.1) }, 20)
+
+    let backgroundNode = ReactDOM.findDOMNode(this.refs.background)
+    backgroundNode.volume = 0
+    setTimeout(() => { this.changeVolume(backgroundNode, 0.0, 1.0) }, 20)
+
   }
 
   render () {
     return (
-      <audio ref='audio' ref="soundfile" muted={!this.props.audio} autoPlay loop> 
-        <source src={this.arcadeSound} type="audio/mpeg" />
-          Your browser does not support the audio content
-      </audio>
+      <div>
+        <audio ref='background' muted={!this.props.audio} autoPlay loop> 
+          <source src={this.audio.background} type="audio/mpeg" />
+            Your browser does not support the audio content
+        </audio>
+        <audio ref='machine' muted={!this.props.audio} autoPlay loop> 
+          <source src={this.audio.machine} type="audio/mpeg" />
+            Your browser does not support the audio content
+        </audio>
+      </div>
     )
   }
 }
